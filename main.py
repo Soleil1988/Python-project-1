c4803fbbb376ec50a73da9ec2a56d4c1913f77ad
OsipovaMaria <mariia.osipova@orioninc.com>
2022-07-29 14:33:57 +0300

    Create template for schedule

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+time = input(f'Введите время ')
+task = input(f'Введите занятие ')
 
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}, your schedule is')
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('Maria')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+with open('./output.txt', 'a') as out_file:
+    out_file.write(f'{time} --- {task}\n')
