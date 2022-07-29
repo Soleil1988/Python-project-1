672aae2d68f01b6c26995abf011d365f1c891c96
OsipovaMaria <mariia.osipova@orioninc.com>
2022-07-29 14:35:36 +0300

    Create template for schedule

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 time = input(f'Введите время ')
 task = input(f'Введите занятие ')
 
-
-def print_hi(name):
-    print(f'Hi, {name}, your schedule is')
-
-
 with open('./output.txt', 'a') as out_file:
     out_file.write(f'{time} --- {task}\n')
